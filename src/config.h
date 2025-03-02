9e9cecff8a4fa3a426fd1c44b4613cb93c440654
ParkerEde <ralf@krusecity.de>
2025-03-02 20:00:22 +0100

    3.0.11-RK Build better neg. Offset2 handling
    3.0.11-RK Build with better neg. Offset2 handling

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <stdint.h>
-#define VERSION "3.0.11RK"
+#define VERSION "3.0.11-RK"
 
 #define DEBUG  // force the MCU to wait for some time for the USB connection; still continue if not connected
 
